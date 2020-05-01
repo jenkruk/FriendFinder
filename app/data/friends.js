45d342e4cb94d7ca2b3be624418c621e36f7e068
Jennifer Kruk <jenkruk@gmail.com>
2020-04-30 17:44:11 -0700

    completed survey and initial friend objects

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,108 @@
 // Header space for easier readability
 
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. 
 
-// You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+```json
+{
+    "name": "Monica Geller",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png",
+    "scores":[
+        2,
+        1,
+        2,
+        1,
+        2,
+        5,
+        5,
+        1,
+        1,
+        5
+      ]
+}
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-// ```
+{
+    "name": "Chandler Bing",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/4/48/Chandler_Bing_portrait.jpg",
+    "scores":[
+        4,
+        3,
+        4,
+        4,
+        1,
+        1,
+        4,
+        5,
+        1,
+        2
+      ]
+}
 
+{
+    "name": "Rachel Greene",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/f/f7/Rachel_Greene.jpg",
+    "scores":[
+        5,
+        5,
+        3,
+        3,
+        2,
+        5,
+        2,
+        4,
+        2,
+        5
+      ]
+    }
+
+{
+    "name": "Ross Geller",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg",
+    "scores":[
+        1,
+        4,
+        5,
+        4,
+        4,
+        3,
+        1,
+        3,
+        3,
+        1
+      ]
+    }
+
+{
+    "name": "Phoebe Buffay",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/3/30/PhoebeBuffay.jpg",
+    "scores":[
+        2,
+        2,
+        1,
+        2,
+        5,
+        2,
+        3,
+        3,
+        4,
+        3
+      ]
+    }
+
+{
+    "name": "Joey Tribbiani",
+    "photo": "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg",
+    "scores":[
+        3,
+        4,
+        1,
+        5,
+        3,
+        4,
+        4,
+        2,
+        5,
+        4
+      ]
+    }
+
+```
